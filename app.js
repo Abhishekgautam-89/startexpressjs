a07e6eeb5c236102b8ba696d09bf5c1e0f99fc7e
Abhishekgautam-89 <abhishek.gautam89@outlook.com>
2022-10-14 22:13:10 +0530

    Using of body-parser

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next)=> {
-    console.log('First middle ware is called');
-    next();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/add-product', (req, res, next)=> {
+    // console.log('First middle ware is called');
+    res.send("<form action = '/product' method='POST'><input type='text' name='title'></br></br> <input type = 'text' name = 'Size'></br></br><button type='submit'>Add</button></form>")    
 });
-app.use((req, res, next)=>{
-    console.log('Second middleware is called');
+
+app.use('/product', (req, res, next)=>{    
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next)=>{
+    // console.log('Second middleware is called');
     res.send('<h1>Hi, from express js</h1>');
 });   
 
